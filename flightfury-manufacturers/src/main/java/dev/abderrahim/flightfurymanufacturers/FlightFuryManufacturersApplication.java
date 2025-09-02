54c08d89340319f9c23c8bf1ea2ed9be132980c4
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-02 03:52:20 +0100

    Add test data by implementing CommandLineRunner's run() method

--- flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplication.java
+++ flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplication.java
@@ -1,13 +1,30 @@
 package dev.abderrahim.flightfurymanufacturers;
 
+import dev.abderrahim.flightfurymanufacturers.models.Manufacturer;
+import dev.abderrahim.flightfurymanufacturers.repositories.ManufacturerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class FlightFuryManufacturersApplication {
+public class FlightFuryManufacturersApplication implements CommandLineRunner {
+
+
+    @Autowired
+    private ManufacturerRepository manufacturerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(FlightFuryManufacturersApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        manufacturerRepository.saveAll(List.of(
+                new Manufacturer(1L,"Northrop Grumman Corporation","USA",1994),
+                new Manufacturer(1L,"Boeing","USA",1916)
+        ));
+    }
 }
