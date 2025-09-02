eeeadc928247164c36ff3e42143cc4a37a565ed3
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-02 02:48:26 +0100

    Add ManufacturerRepository.java

---
+++ flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/repositories/ManufacturerRepository.java
@@ -0,0 +1,7 @@
+package dev.abderrahim.flightfurymanufacturers.repositories;
+
+import dev.abderrahim.flightfurymanufacturers.models.Manufacturer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ManufacturerRepository extends MongoRepository<Manufacturer, String> {
+}
