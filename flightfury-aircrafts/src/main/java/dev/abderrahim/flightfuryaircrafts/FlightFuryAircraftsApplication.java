c92554781681f5b166263cd511a62ee8146767fd
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-02 02:45:15 +0100

    Refactor projects dependencies in the root pom.xml

--- flightfury-aircrafts/src/main/java/dev/abderrahim/flightfuryaircrafts/FlightFuryAircraftsApplication.java
+++ flightfury-aircrafts/src/main/java/dev/abderrahim/flightfuryaircrafts/FlightFuryAircraftsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightfuryAircraftsApplication {
+public class FlightFuryAircraftsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlightfuryAircraftsApplication.class, args);
+        SpringApplication.run(FlightFuryAircraftsApplication.class, args);
     }
 
 }
