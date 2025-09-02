531ae058754e785de876b58289ac01a674497dbb
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-02 02:19:58 +0100

    Add a new module for manufacturers service

---
+++ flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/FlightfuryManufacturersApplication.java
@@ -0,0 +1,13 @@
+package dev.abderrahim.flightfurymanufacturers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightfuryManufacturersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightfuryManufacturersApplication.class, args);
+    }
+
+}
---
+++ flightfury-manufacturers/src/test/java/dev/abderrahim/flightfurymanufacturers/FlightfuryManufacturersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abderrahim.flightfurymanufacturers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightfuryManufacturersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
