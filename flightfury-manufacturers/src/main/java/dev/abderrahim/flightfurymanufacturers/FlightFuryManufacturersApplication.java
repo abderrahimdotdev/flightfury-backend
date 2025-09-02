a3ce7196aa419a30e73fee2eb2a9f1f064e0d410
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-02 02:29:03 +0100

    Fix a typo in the main class name

--- flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplication.java
+++ flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightfuryManufacturersApplication {
+public class FlightFuryManufacturersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlightfuryManufacturersApplication.class, args);
+        SpringApplication.run(FlightFuryManufacturersApplication.class, args);
     }
 
 }
--- flightfury-manufacturers/src/test/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplicationTests.java
+++ flightfury-manufacturers/src/test/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlightfuryManufacturersApplicationTests {
+class FlightFuryManufacturersApplicationTests {
 
     @Test
     void contextLoads() {
