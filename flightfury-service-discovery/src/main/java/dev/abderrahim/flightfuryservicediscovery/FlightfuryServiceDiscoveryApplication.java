3e6847441f2caed1bebff7442d8df05c7db4a49d
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-08 14:58:55 +0100

    Add service discovery module

---
+++ flightfury-service-discovery/src/main/java/dev/abderrahim/flightfuryservicediscovery/FlightfuryServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package dev.abderrahim.flightfuryservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightfuryServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightfuryServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ flightfury-service-discovery/src/test/java/dev/abderrahim/flightfuryservicediscovery/FlightfuryServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abderrahim.flightfuryservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightfuryServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
