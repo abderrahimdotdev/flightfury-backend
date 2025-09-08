24c832f0337e4f17a90d1d7b1d0722e1cdece432
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-08 14:43:30 +0100

    Change data type of primary key from String to Long in ManufacturerRepository

--- flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/repositories/ManufacturerRepository.java
+++ flightfury-manufacturers/src/main/java/dev/abderrahim/flightfurymanufacturers/repositories/ManufacturerRepository.java
@@ -3,5 +3,5 @@ package dev.abderrahim.flightfurymanufacturers.repositories;
 import dev.abderrahim.flightfurymanufacturers.models.Manufacturer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ManufacturerRepository extends MongoRepository<Manufacturer, String> {
+public interface ManufacturerRepository extends MongoRepository<Manufacturer, Long> {
 }
