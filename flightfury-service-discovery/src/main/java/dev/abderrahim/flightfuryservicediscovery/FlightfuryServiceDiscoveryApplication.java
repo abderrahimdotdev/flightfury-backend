b7edd79df0f6b87c186591a34842373dab8ac91e
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-08 15:22:23 +0100

    Set up initial configuration of service discovery module

--- flightfury-service-discovery/src/main/java/dev/abderrahim/flightfuryservicediscovery/FlightfuryServiceDiscoveryApplication.java
+++ flightfury-service-discovery/src/main/java/dev/abderrahim/flightfuryservicediscovery/FlightfuryServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package dev.abderrahim.flightfuryservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FlightfuryServiceDiscoveryApplication {
 
     public static void main(String[] args) {
