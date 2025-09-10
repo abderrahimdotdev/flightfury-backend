4668f401b1b978936e2a072321041defaf35f971
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-10 13:28:27 +0100

    Add gateway module

---
+++ flightfury-gateway/src/main/java/dev/abderrahim/flightfurygateway/FlightfuryGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.abderrahim.flightfurygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightfuryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightfuryGatewayApplication.class, args);
+    }
+
+}
---
+++ flightfury-gateway/src/test/java/dev/abderrahim/flightfurygateway/FlightfuryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abderrahim.flightfurygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightfuryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
