647867afcd5fc17403f2f5314b7fa78a98becf13
Abderrahim <abderrahimdotdev@gmail.com>
2025-09-11 15:45:34 +0100

    Add unit tests for manufacturers module

--- flightfury-manufacturers/src/test/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplicationTests.java
+++ flightfury-manufacturers/src/test/java/dev/abderrahim/flightfurymanufacturers/FlightFuryManufacturersApplicationTests.java
@@ -1,13 +1,33 @@
 package dev.abderrahim.flightfurymanufacturers;
 
+import dev.abderrahim.flightfurymanufacturers.controllers.ManufacturerController;
+import dev.abderrahim.flightfurymanufacturers.models.Manufacturer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FlightFuryManufacturersApplicationTests {
 
+    private final ManufacturerController manufacturerController;
+
+    @Autowired
+    public FlightFuryManufacturersApplicationTests(ManufacturerController manufacturerController) {
+        this.manufacturerController = manufacturerController;
+    }
+
     @Test
-    void contextLoads() {
+    void findOneTest() {
+        long lookupId = 2;
+        Manufacturer boeing = manufacturerController.findOne(lookupId).get();
+
+        assertAll(
+                () -> assertEquals(lookupId, boeing.getId()),
+                () -> assertEquals("Boeing", boeing.getName())
+        );
     }
 
 }
